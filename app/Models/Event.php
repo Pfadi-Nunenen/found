ac1689c3afa0b8efcc5629198236d490346afe54
Caspar Brenneisen <github@brenneisen.me>
2022-06-21 18:22:07 +0200

    Update Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    use HasFactory;
+
     protected $table = 'events';
 
-    protected $fillable = ['event_name', 'event_date', 'event_active'];
+    protected $fillable = [
+        'event_name',
+        'event_date',
+        'evento_Active'
+    ];
 }
