9b4fef7913363875affd6724ccb566c28857ef16
Caspar Brenneisen <github@brenneisen.me>
2022-06-21 18:12:08 +0200

    Create 2022_06_21_161041_create_items_table.php

---
+++ database/migrations/2022_06_21_161041_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('item_identifier');
+            $table->string('item_name');
+            $table->string('item_color')->nullable();
+            $table->string('item_size')->nullable();
+            $table->boolean('item_returned')->default(false);
+            $table->integer('item_price')->nullable();
+            $table->boolean('item_sold')->default(false);
+            $table->string('item_img')->nullable();
+            $table->foreignId('event_id')->nullable();
+            $table->foreignId('group_id')->nullable();
+            $table->foreignId('customer_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
