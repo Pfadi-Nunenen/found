0e8db3e0478469bbe4e86b5cb13342c15bcf02b7
Caspar Brenneisen <github@brenneisen.me>
2022-06-21 18:16:45 +0200

    Create 2022_06_21_161332_create_fks.php

---
+++ database/migrations/2022_06_21_161332_create_fks.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('set null');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('set null');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('set null');
+        });
+    }
+};
