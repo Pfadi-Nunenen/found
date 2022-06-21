cbb1f0ac19f307f3ce3ff11adf343383216fc521
Caspar Brenneisen <github@brenneisen.me>
2022-06-21 18:20:37 +0200

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,16 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    use HasFactory;
+
     protected $table = 'customers';
 
-    protected $fillable = ['customer_name', 'customer_mail'];
+    protected $fillable = [
+        'customer_name',
+        'customer_mail'
+    ];
 
-    public function item()
-    {
-        return $this->hasOne('App\Models\Item');
+    public function item() {
+        return $this->hasOne(App\Model\Items::class);
     }
 }
