1f28f7ed4e121c4c3351b25ec0c5f5266cb3d3e0
Caspar Brenneisen <github@brenneisen.me>
2022-06-21 18:29:43 +0200

    Update Group.php

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    use HasFactory;
+
     protected $table = 'groups';
 
-    protected $fillable = ['group_name', 'group_active'];
+    protected $fillable = [
+        'group_name',
+        'group_active'
+    ];
 }
